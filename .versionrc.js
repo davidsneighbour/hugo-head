3bac47fea0101f5b5def34cd2039114ed4f9229d
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-09-05 19:26:39 +0700

    chore: cleanup repo configuration

--- .versionrc.js
+++ .versionrc.js
@@ -1,13 +1,16 @@
 const defaultStandardVersion = require("@davidsneighbour/config/standard-version");
 
 const localStandardVersion = {
-  bumpFiles: [
-    ...defaultStandardVersion.bumpFiles,
-    { filename: "data/dnb/head/build.json", type: "json" },
-  ],
+	bumpFiles: [
+		...defaultStandardVersion.bumpFiles,
+		{ filename: "data/dnb/head/build.json", type: "json" },
+	],
+	skip: {
+		changelog: true
+	}
 };
 
 module.exports = {
-  ...defaultStandardVersion,
-  ...localStandardVersion,
+	...defaultStandardVersion,
+	...localStandardVersion,
 };
